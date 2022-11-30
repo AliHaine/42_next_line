af8f67e4a354da70a87664a0cbdf59a3cd99e0d5
Ali Yagmur <ayagmur@k0r3p11.42mulhouse.fr>
2022-11-30 17:31:51 +0100

    fix + bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayagmur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/20 12:56:57 by ayagmur           #+#    #+#             */
-/*   Updated: 2022/11/20 12:56:58 by ayagmur          ###   ########.fr       */
+/*   Created: 2022/11/30 17:23:35 by ayagmur           #+#    #+#             */
+/*   Updated: 2022/11/30 17:23:39 by ayagmur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 2
